bcb1216f12548e63cc1f472b1a19dff7b70a69af
G'lek Tarssza <glek@glektarssza.com>
2026-08-19 09:29:00 -0600

    Clean up `annotations` package.

--- src/main/java/com/glektarssza/utility_library/annotations/ParametersNotNullByDefault.java
+++ src/main/java/com/glektarssza/utility_library/annotations/ParametersNotNullByDefault.java
@@ -1,4 +1,4 @@
-package com.glektarssza.utility_library.api.annotations;
+package com.glektarssza.utility_library.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
--- src/main/java/com/glektarssza/utility_library/api/annotations/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * A package containing useful annotations for the mod.
- */
-package com.glektarssza.utility_library.api.annotations;
